5176dd3ca284c560c7ae7d1108cab04c0428fa0c
Abdou Sadou <sadou.m.abderrahmane@gmail.com>
2026-04-02 00:28:44 +0100

    fix: set basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "", // Change this if deploying to a subpath like /fitgirl_downloader
+  basePath: "/fitgirl-downloader",
 };
 
 export default nextConfig;
